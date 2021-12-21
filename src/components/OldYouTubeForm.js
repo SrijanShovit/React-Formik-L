502410a0023a7381fb27f5d752bc824f86a54ada
Srijan Shovit <srijanshovit@gmail.com>
2021-12-21 23:13:28 +0530

    reducing boilerplate using get field props

--- src/components/OldYouTubeForm.js
+++ src/components/OldYouTubeForm.js
@@ -12,6 +12,29 @@ const onSubmit = values => {
     console.log('Form data',values)
 }
 
+const validate= values=> {
+    //Imp points about validate
+    //1.should return an object
+    //2.Properties of this object should be same as properties of values
+    //3.Values for these keys should be string indicating error messages for these keys
+
+    let errors = {}
+
+    if(!values.name){
+        errors.name = 'Required'
+    }
+
+    if (!values.email){
+        errors.email = 'Required'
+    }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        errors.email = 'Invalid email format'
+    }
+
+    if (!values.channel){
+        errors.channel = 'Required'
+    }
+    return errors
+}
 
 const validationSchema = Yup.object({
     name: Yup.string().required('Required'),
@@ -19,7 +42,7 @@ const validationSchema = Yup.object({
     channel : Yup.string().required('Required'),
 })
 
-function YoutubeForm() {
+function OldYoutubeForm() {
 
    
     const formikobj = useFormik({
@@ -39,7 +62,9 @@ function YoutubeForm() {
 
                 <label htmlFor="name">Name</label>
                 <input type="text" id="name" name="name" 
-               {...formikobj.getFieldProps('name')}
+                onChange={formikobj.handleChange}
+                onBlur={formikobj.handleBlur} 
+                value={formikobj.values.name}
                 />
                 {formikobj.touched.name && formikobj.errors.name ? <div className="error">{formikobj.errors.name}</div> : null}
                 </div>
@@ -47,7 +72,9 @@ function YoutubeForm() {
                 <div className="form-control">
                 <label htmlFor="email">E-mail</label>
                 <input type="email" id="email" name="email" 
-                 {...formikobj.getFieldProps('email')}
+                onChange={formikobj.handleChange}
+                onBlur={formikobj.handleBlur}  //keep track of visited fields
+                value={formikobj.values.email}
                 />
                  {formikobj.touched.email && formikobj.errors.email ? <div className="error">{formikobj.errors.email}</div> : null}
                 </div>
@@ -56,7 +83,9 @@ function YoutubeForm() {
 
                 <label htmlFor="channel">Channel</label>
                 <input type="text" id="channel" name="channel" 
-                {...formikobj.getFieldProps('channel')}
+                onChange={formikobj.handleChange}
+                onBlur={formikobj.handleBlur} 
+                value={formikobj.values.channel}
                 />
                  {formikobj.touched.channel && formikobj.errors.channel ? <div className="error">{formikobj.errors.channel}</div> : null}
                 </div>
@@ -67,4 +96,4 @@ function YoutubeForm() {
     )
 }
 
-export default YoutubeForm
+export default OldYoutubeForm
