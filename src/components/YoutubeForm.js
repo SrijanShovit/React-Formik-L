259c8d224eebf43cfb534892cfcf07ff66a25714
Srijan Shovit <srijanshovit@gmail.com>
2021-12-21 23:08:12 +0530

    validation schema using yup

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useFormik} from 'formik'
+import * as Yup from 'yup'
 
 const initialValues = {   
     name:'Srijan',
@@ -35,6 +36,11 @@ const validate= values=> {
     return errors
 }
 
+const validationSchema = Yup.object({
+    name: Yup.string().required('Required'),
+    email : Yup.string().email("Invalid email format").required("Required"),
+    channel : Yup.string().required('Required'),
+})
 
 function YoutubeForm() {
 
@@ -42,7 +48,8 @@ function YoutubeForm() {
     const formikobj = useFormik({
         initialValues,        
         onSubmit,
-        validate 
+        // validate 
+        validationSchema
     })
 
     console.log('Form errors',formikobj.errors)
