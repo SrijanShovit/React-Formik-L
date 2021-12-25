fd6937a721660ca05124225175172e2c7a6785e7
Srijan Shovit <srijanshovit@gmail.com>
2021-12-25 20:42:49 +0530

    Login Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import YoutubeForm from './components/YoutubeForm'
-import FormikContainer from './controlcomponents/FormikContainer';
+// import YoutubeForm from './components/YoutubeForm'
+// import FormikContainer from './controlcomponents/FormikContainer';
+import LoginForm from './controlcomponents/LoginForm';
 
 function App() {
   return (
     <div className="App">
      {/* <YoutubeForm/> */}
-     <FormikContainer/>
+     {/* <FormikContainer/> */}
+     <LoginForm/>
     </div>
   );
 }
