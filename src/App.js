e53cdddcd9f8c28f655745f8609e4f2657904e12
Srijan Shovit <srijanshovit@gmail.com>
2021-12-25 22:09:27 +0530

    Course Enrollment Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import EnrollmentForm from './controlcomponents/EnrollmentForm';
 // import YoutubeForm from './components/YoutubeForm'
 // import FormikContainer from './controlcomponents/FormikContainer';
 // import LoginForm from './controlcomponents/LoginForm';
-import RegistrationForm from './controlcomponents/RegistrationForm';
+// import RegistrationForm from './controlcomponents/RegistrationForm';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
      {/* <YoutubeForm/> */}
      {/* <FormikContainer/> */}
      {/* <LoginForm/> */}
-     <RegistrationForm/>
+     {/* <RegistrationForm/> */}
+     <EnrollmentForm/>
     </div>
   );
 }
